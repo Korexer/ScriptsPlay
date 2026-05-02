a62a6b2e8c5ae3186b1fb259e92d270b9d0de541
akore <akore@example.com>
2026-05-02 14:13:40 +0100

    Move Supabase credentials to environment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lwyhmmwaakwzdmdkanxq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3eWhtbXdhYWt3emRtZGthbnhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTM1MzksImV4cCI6MjA4ODU4OTUzOX0.e7V36zfQlneQTEsWWtovIUhlkUBquEPV2BKdfKR8_Cs';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
